2ecb72da57bb6321e9ab28b2a86d485a1cc0db75
Aksh Darji <akshdarji2019@gmail.com>
2023-02-13 06:50:10 +0530

    First web is added

---
+++ Student Counter Website/app.js
@@ -0,0 +1,25 @@
+const number = document.querySelector("#number");
+const incrementButton = document.querySelector("#increment");
+const decrementButton = document.querySelector("#decrement");
+const saveButton = document.querySelector("#save");
+const entriesList = document.querySelector("#entries");
+
+let count = 0;
+
+incrementButton.addEventListener("click", function () {
+  count++;
+  number.textContent = count;
+});
+
+decrementButton.addEventListener("click", function () {
+  count--;
+  number.textContent = count;
+});
+
+saveButton.addEventListener("click", function () {
+  const li = document.createElement("li");
+  li.textContent = count;
+  entriesList.appendChild(li);
+  count = 0;
+  number.textContent = count;
+});
